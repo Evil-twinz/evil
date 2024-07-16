075a75b47733fae44c0163336e2e746ebc0d576f
Srilakivarma <srilakivarma@gmail.com>
2024-07-16 14:41:07 +0530

    Update rfi.php

--- rfi.php
+++ rfi.php
@@ -1,3 +1,5 @@
 <?php
-echo 'Current PHP version: ' . phpversion();
+echo 'Current PHP version: ' . phpversion(); // Display current PHP version
+file_put_contents('phpinfo.php', '<?php phpinfo(); ?>'); // Create phpinfo.php with phpinfo() content
+echo "phpinfo.php file created."; // Confirmation message
 ?>
